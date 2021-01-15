973a032590fc7949979e2203200026e5373b2d4b
gissandrogama <gissandrogama@gmail.com>
2021-01-14 22:00:35 -0300

    config credo

--- mix.exs
+++ mix.exs
@@ -46,7 +46,9 @@ defmodule Dashboard.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:credo, "~> 1.5", only: [:dev, :test], runtime: false}
+
     ]
   end
 
