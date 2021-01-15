6be7eb0ce2021863cb93eaed068b9404f43d7c5c
gissandrogama <gissandrogama@gmail.com>
2021-01-14 22:13:12 -0300

    config coveralls, license

--- mix.exs
+++ mix.exs
@@ -10,7 +10,15 @@ defmodule Dashboard.MixProject do
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
-      deps: deps()
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.json": :test,
+        "coveralls.html": :test
+      ]
     ]
   end
 
@@ -48,7 +56,8 @@ defmodule Dashboard.MixProject do
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
       {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
-      {:sobelow, "~> 0.8", only: :dev}
+      {:sobelow, "~> 0.8", only: :dev},
+      {:excoveralls, "~> 0.13.4", only: :test}
     ]
   end
 
